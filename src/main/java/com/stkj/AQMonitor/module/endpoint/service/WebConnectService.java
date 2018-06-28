ec1528afa78728baa2f102e59a6e41d1c5efc1b5
wps <bbsctor@126.com>
2018-06-28 18:00:04 +0800

    删除无效文件。

--- src/main/java/com/stkj/AQMonitor/module/endpoint/service/WebConnectService.java
+++
@@ -1,9 +0,0 @@
-package com.stkj.AQMonitor.module.endpoint.service;
-
-public interface WebConnectService {
-	
-	void connect();	 
-	String SendAndGetMessage(String message);
-	void closeconnect();
-
-}
